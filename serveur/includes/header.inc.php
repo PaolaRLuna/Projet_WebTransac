b7b0c2bdf221c05da77134a8a5db18f0d03abbe4
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2023-09-12 11:32:51 -0400

    Creation dossier includes dans serveur

---
+++ serveur/includes/header.inc.php
@@ -0,0 +1,4 @@
+<div>
+    <button onClick="requeteServeurSynchrone();">ENVOYER</button>
+    <button onClick="document.getElementbyId('form2').style.display='block';">ENVOYER AVEC DONNÉES</button>
+</div>
