25c3cfb5ffc5c7995610b93d7f5f1d23b973d202
ahguerram <116131865+ahguerram@users.noreply.github.com>
2023-10-18 21:01:02 -0400

    maj

--- serveur/produit/DaoProduit.php
+++ serveur/produit/DaoProduit.php
@@ -23,25 +23,25 @@ class DaoProduit {
 		return self::$modelProduit;
 	}
 	
-	function MdlF_Enregistrer(Produit $produit):string {
+	// function MdlF_Enregistrer(Produit $produit):string {
              
-        $connexion =  Connexion::getConnexion();
+    //     $connexion =  Connexion::getConnexion();
         
-        $requette="INSERT INTO produits VALUES(0,?,?,?,?)";
-        try{
-            $donnees = [$produit->getTitre(),$produit->getDuree(),$produit->getRealisateur(),$produit->getPochette()];
-            $stmt = $connexion->prepare($requette);
-            $stmt->execute($donnees);
-            $this->reponse['OK'] = true;
-            $this->reponse['msg'] = "Produit bien enregistre";
-        }catch (Exception $e){
-            $this->reponse['OK'] = false;
-            $this->reponse['msg'] = "Probléme pour enregistrer le produit";
-        }finally {
-          unset($connexion);
-          return json_encode($this->reponse);
-        }
-    }
+    //     $requette="INSERT INTO produits VALUES(0,?,?,?,?)";
+    //     try{
+    //         $donnees = [$produit->getTitre(),$produit->getDuree(),$produit->getRealisateur(),$produit->getPochette()];
+    //         $stmt = $connexion->prepare($requette);
+    //         $stmt->execute($donnees);
+    //         $this->reponse['OK'] = true;
+    //         $this->reponse['msg'] = "Produit bien enregistre";
+    //     }catch (Exception $e){
+    //         $this->reponse['OK'] = false;
+    //         $this->reponse['msg'] = "Probléme pour enregistrer le produit";
+    //     }finally {
+    //       unset($connexion);
+    //       return json_encode($this->reponse);
+    //     }
+    // }
 	
     function MdlP_getAll():string {
         $requete="SELECT * FROM produits";
@@ -82,5 +82,49 @@ class DaoProduit {
           return json_encode($this->reponse);
         }
     }
+
+//Houssam****************************************************
+
+
+function MdlP_getByCategory($categorie): string {
+    $requete = "SELECT * FROM produits WHERE categorie = ?";
+    
+    try {
+        $instanceModele = modeleDonnees::getInstanceModele();
+        $stmt = $instanceModele->executer($requete, [$categorie]);
+        $this->reponse['OK'] = true;
+        $this->reponse['msg'] = "Opération réussie";
+        $this->reponse['listeProduits'] = array();
+        while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+            $this->reponse['listeProduits'][] = $ligne;
+        }
+    } catch (Exception $e) {
+        $this->reponse['OK'] = false;
+        $this->reponse['msg'] = "Problème pour obtenir les données des produits par catégorie";
+    } finally {
+        return json_encode($this->reponse);
+    }
 }
+
+
+function rechercherParMotCle(array $params): string {
+    $instanceModele = modeleDonnees::getInstanceModele();
+    $motCle = $params['motCle'];
+
+    try {
+        $requete = "SELECT * FROM produits WHERE nom LIKE :motCle OR ingredients LIKE :motCle";
+        $stmt = $instanceModele->executer($requete, [':motCle' => '%' . $motCle . '%']);
+        $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return json_encode(["OK" => true, "msg" => "Recherche réussie", "resultats" => $resultats]);
+    } catch (Exception $e) {
+        return json_encode(["OK" => false, "msg" => "Problème de recherche"]);
+    }
+}
+
+
+
+}
+
+
 ?>
